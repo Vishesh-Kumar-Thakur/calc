e35506f45a39a11f51b575aa2424c3e89af104ea
Vishesh-Kumar-Thakur <51586871+Vishesh-Kumar-Thakur@users.noreply.github.com>
2019-06-12 11:55:30 +0530

    Add files via upload

---
+++ CALCUL~1.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+double a, b, result;
+int choice;
+clrscr();
+result = 0;
+printf("Enter two nubers");
+scanf("%lf%lf", &a,&b);
+printf("Enter your choice: \n 1 for adition\n 2 for subtraction\n 3 for multiplication\n for quotiant \n for remainder");
+scanf("%d", &choice);
+switch(choice){
+case 1: result = a+b;
+	printf("The sum of %lf and %lf is: %lf",a,b,result);
+	break;
+case 2: result = a-b;
+	printf("The difference of %lf and %lf is: %lf",a,b,result);
+	break;
+case 3: result = a*b;
+	printf("The multiplication of %lf and %lf is: %lf",a,b,result);
+	break;
+case 4: result = a/b;
+	printf("The quotiant of %lf and %lf is: %lf",a,b,result);
+	break;
+case 5: break;
+default:"invalid choice";
+}
+getch();
+}
+
